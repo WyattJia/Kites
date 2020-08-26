6d0987822be704527d94345369925fcc66a395aa
wellls <wellschuan@gmail.com>
2020-08-26 23:51:40 +0800

    Update raft build gradle.

--- raft/build.gradle.kts
+++ raft/build.gradle.kts
@@ -1,25 +1,45 @@
+
 plugins {
     kotlin("jvm") version "1.3.72"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     kotlin("plugin.serialization") version "1.3.72"
 }
 
-repositories {
+group = "raft.kites"
+version = "0.0.1"
 
+repositories {
+    mavenCentral()
+    jcenter()
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
     implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib", "1.2.31"))
     testImplementation("junit:junit:4.12")
+
+
 }
 
-
-buildscript {
-    repositories { jcenter() }
-
-    dependencies {
-        val kotlinVersion = "1.3.72"
-        classpath(kotlin("gradle-plugin", version = kotlinVersion))
-        classpath(kotlin("serialization", version = kotlinVersion))
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
+        kotlinOptions.allWarningsAsErrors = true
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
+        kotlinOptions.allWarningsAsErrors = true
+    }
+    withType<Test> {
+        useJUnitPlatform()
     }
 }
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
