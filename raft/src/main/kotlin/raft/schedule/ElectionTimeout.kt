cbbd6c05054c4fa31971405740115ca5f47c25cc
wellls <wellschuan@gmail.com>
2020-09-02 23:28:48 +0800

    Raft core: define schedulers.

---
+++ raft/src/main/kotlin/raft/schedule/ElectionTimeout.kt
@@ -0,0 +1,12 @@
+package raft.schedule
+
+import java.util.concurrent.ScheduledFuture
+
+abstract class ElectionTimeout {
+    // todo find kotlin's logger best practice
+//    val logger: Logger = LoggerFactory.getLogger(javaClass<ElectionTimeout>())
+    val None:ElectionTimeout = ElectionTimeout(NullSchedulerFuture())
+
+    // kotlin scheduledFuture usage
+    val scheduledFuture: ScheduledFuture<?>
+}
---
+++ raft/src/main/kotlin/raft/schedule/LogReplicationTask.kt
@@ -0,0 +1,5 @@
+package raft.schedule
+
+class LogReplicationTask {
+
+}
