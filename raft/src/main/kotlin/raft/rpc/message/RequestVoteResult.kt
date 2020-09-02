7fbca86dd011b80399324df26867e4c29e13206a
wellls <wellschuan@gmail.com>
2020-09-02 23:29:23 +0800

    Raft core: define rpc message request vote rpc and result class.

---
+++ raft/src/main/kotlin/raft/rpc/message/RequestVoteResult.kt
@@ -0,0 +1,24 @@
+package raft.rpc.message
+
+import java.io.Serializable
+
+class RequestVoteResult(private val term: Int, private val voteGranted: Boolean):Serializable {
+    init {
+        term
+        voteGranted
+    }
+
+    fun getTerm():Int {
+        return term
+    }
+
+    fun isVoteGranted():Boolean {
+        return voteGranted
+    }
+
+    override fun toString(): String {
+       return "RequestVoteResult{" + "term=" + term +
+                ", voteGranted=" + voteGranted +
+                '}'
+    }
+}
---
+++ raft/src/main/kotlin/raft/rpc/message/RequestVoteRpc.kt
@@ -0,0 +1,21 @@
+package raft.rpc.message
+
+import raft.node.NodeId.NodeId
+import kotlin.properties.Delegates
+
+class RequestVoteRpc {
+    var term by Delegates.notNull<Int>()
+    lateinit var candidateId: NodeId
+    var lastLogIndex: Int = 0
+    var lastLogTerm: Int = 0
+
+    override fun toString(): String {
+        return "RequestVoteRpc{" +
+                "candidateId=" + candidateId +
+                ", lastLogIndex=" + lastLogIndex +
+                ", lastLogTerm=" + lastLogTerm +
+                ", term=" + term +
+                '}'
+    }
+
+}
