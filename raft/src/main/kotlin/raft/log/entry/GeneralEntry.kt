b8628358bf9072c9d05ada4bc3358336b6c77faa
wellls <wellschuan@gmail.com>
2020-10-14 02:22:12 +0800

    Raft log: Create general entry class.

---
+++ raft/src/main/kotlin/raft/log/entry/GeneralEntry.kt
@@ -0,0 +1,14 @@
+package raft.log.entry
+
+import raft.log.entry.Entry.Companion.KIND_GENERAL
+
+class GeneralEntry(index: Int, term: Int, override val commandBytes: ByteArray) :
+    AbstractEntry(KIND_GENERAL, index, term) {
+
+    override fun toString(): String {
+        return "GeneralEntry{" +
+                "index=" + index +
+                ", term=" + term +
+                '}'
+    }
+}
